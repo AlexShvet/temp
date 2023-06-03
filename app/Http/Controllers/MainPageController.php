4c86251ce7967cc718a7205fb72d1362fe4eba25
AlexShvet <malifey39@gmail.com>
2023-06-03 14:03:04 +0700

    json отдает норм  пагинацию доделать

--- app/Http/Controllers/MainPageController.php
+++ app/Http/Controllers/MainPageController.php
@@ -13,23 +13,50 @@ class MainPageController extends Controller
 {
     public function index()
     {
-        
+
         $user = User::paginate(10)->toArray();
         $social_media = social_media::paginate(10)->toArray();
         $phone = Phone::paginate(10)->toArray();
         $email = Email::paginate(10)->toArray();
         $avatar = Avatar::paginate(10)->toArray();
         
-        for ($i=0; $i < 10; $i++) { 
-            $all[] = $user['data'][$i];
-            $all[] = $social_media['data'][$i];
-            $all[] = $phone['data'][$i];
-            $all[] = $email['data'][$i];
-            $all[] = $avatar['data'][$i];
+        $finaledJson = '[';
+        for ($i = 0; $i < 10; $i++) {
+            $finaledJson .= '{';
+            foreach ($user['data'][$i] as $key => $value) {
+                if ($key != 'created_at' and $key != 'updated_at') {
+                    $finaledJson .= '"' . $key . '":"' . $value . '",';
+                }
+            }
+            foreach ($social_media['data'][$i] as $key => $value) {
+                if ($key != 'created_at' and $key != 'updated_at' AND $key != 'id' AND $key != 'users_id') {
+                    $finaledJson .= '"' . $key . '":"' . $value . '",';
+                }
+            }
+           
+            foreach ($phone['data'][$i] as $key => $value) {
+                if ($key != 'created_at' and $key != 'updated_at' AND $key != 'id' AND $key != 'users_id') {
+                    $finaledJson .= '"' . $key . '":"' . $value . '",';
+                }
+            }
+            foreach ($email['data'][$i] as $key => $value) {
+                if ($key != 'created_at' and $key != 'updated_at' AND $key != 'id' AND $key != 'users_id') {
+                    $finaledJson .= '"' . $key . '":"' . $value . '",';
+                }
+            }
+            foreach ($avatar['data'][$i] as $key => $value) {
+                if ($key != 'created_at' and $key != 'updated_at' AND $key != 'id' AND $key != 'users_id') {
+                    $finaledJson .= '"' . $key . '":"' . $value . '",';
+                }
+            }
+            $finaledJson = mb_substr($finaledJson, 0, -1);
+            $finaledJson .= '},';
         }
-        $finaledJson = json_encode($all);
-        //$finaledJson = str_replace(',', ',<br>', $finaledJson);// для себя, что б удобнее проверять
-      
+        $finaledJson = mb_substr($finaledJson, 0, -1);
+        $finaledJson .= ']';
+        
+        //$finaledJson = str_replace(',', ',<br>', $finaledJson); // для себя, что б удобнее проверять
+       
         return view('all', ['all' => $finaledJson]);
     }
 }
