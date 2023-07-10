1d1e0bc176aae1f6b0636cae6d94b93e79106775
AlexShvet <malifey39@gmail.com>
2023-07-10 19:33:20 +0700

    все работает. Код из видео индуса не исправлен и тоже работает

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::paginate(10);
+        
+        $a = UserResource::collection($users);
+        //dd($a);
+        return UserResource::collection($users);
+    }
+}
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+
+import { createApp } from "vue/dist/vue.esm-bundler.js";
+import PostIndex from "./components/Posts/PostIndex.vue";
+const app = createApp({});
+app.component("post-index", PostIndex);
+
+app.mount("#app");
